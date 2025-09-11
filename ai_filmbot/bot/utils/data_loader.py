7b6b530f08dfee255b73883c730d7a2d251f2536
ningagoo <vladosikgv@gmail.com>
2025-09-11 15:16:07 +0300

    Delete ai_filmbot directory

--- ai_filmbot/bot/utils/data_loader.py
+++
@@ -1,9 +0,0 @@
-import json
-
-def get_movie_by_code(code: str, db_path="bot/data/media/movies.json") -> dict | None:
-    try:
-        with open(db_path, "r", encoding="utf-8") as f:
-            data = json.load(f)
-        return data.get(code)
-    except Exception:
-        return None
